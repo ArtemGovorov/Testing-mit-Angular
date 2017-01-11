a1126583f023a60c400efa9649aefb1cc593c392
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-01-11 12:59:27 +0100

    Added dummy test

---
+++ app/components/allPersons/person-detail.component.spec.ts
@@ -0,0 +1,14 @@
+describe('MyList Tests', () => {
+    let list: string[];
+
+    beforeEach(() => {
+        list = [];
+        list.push("Thomas");
+    });
+
+    it('Should get 5 dogs', () => {
+        // list.ngOnInit();
+
+        expect(list.length).toBe(1);
+    });
+});
