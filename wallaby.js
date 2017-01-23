371a64fd4399901ea7c39a4ee4807a5ce1c2f512
Thomas Bandixen <thomas@bandixen.ch>
2017-01-23 14:08:42 +0100

    included

--- wallaby.js
+++ wallaby.js
@@ -2,10 +2,10 @@ module.exports = function (wallaby) {
 
     return {
         files: [
-            //     { pattern: 'node_modules/core-js/client/shim.js', instrument: false },
-            // { pattern: 'node_modules/zone.js/dist/zone.js', instrument: false },
-            //     { pattern: 'node_modules/reflect-metadata/Reflect.js', instrument: false },
-            //     { pattern: 'node_modules/systemjs/dist/system.src.js', instrument: false },
+            { pattern: 'node_modules/core-js/client/shim.js', instrument: false },
+            { pattern: 'node_modules/zone.js/dist/zone.js', instrument: false },
+            { pattern: 'node_modules/reflect-metadata/Reflect.js', instrument: false },
+            { pattern: 'node_modules/systemjs/dist/system.src.js', instrument: false },
             { pattern: 'systemjs.config.js', instrument: false },
             { pattern: 'systemjs.config.extras.js', instrument: false },
             { pattern: 'app/**/*.spec.ts', ignore: true },
